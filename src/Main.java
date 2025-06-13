17f7050fe9034ec69a9f57db4d69248bf4b1ad2d
이은정 <ej2grateful@gmail.com>
2025-06-13 15:41:34 +0900

    2

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+class CPU {
+    public int register = 0;
+    public int[] memory = { 5, 10, 0};
+
+    private static final int LOAD = 1;
+    private static final int SAVE = 2;
+    private static final int ADD  = 3;
+    private static final int SUB  = 4;
+    private static final int HALT = 5;
+
+    private int[][] program ={
+            { LOAD, 0 }, // register = memory[0]
+            { ADD, 1 }, // register += memory[1]
+            { SAVE, 2 }, // memory[2] = register
+            { HALT, 0 }
+    };
+
+    public void executeProgram() {
+        int pc = 0;
+
+        while(true) {
+           int opcode  = program[pc][0];  //명령어
+           int operand = program[pc][1];
+
+           switch (opcode) {
+               case LOAD:
+                   register = memory[operand];
+                   System.out.println("MOV: Loading Memory[" + opcode
+                           + " (" + memory[0] + ") into register.]");
+                   break;
+               case SAVE:
+                   register = memory[operand];
+                   System.out.println("MOV: Loading Memory[" + opcode
+                                   + " (" + memory[0] + ") into register.]");
+                   break;
+           }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
